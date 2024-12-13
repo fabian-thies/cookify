38cb28cd1d0313af55cf4e764dbbb0fc9c9da4b2
fabianthies <44522183+fabian-thies@users.noreply.github.com>
2024-12-13 01:20:48 +0100

    feat: implement createUser function

---
+++ src/lib/server/db/queries/user.ts
@@ -0,0 +1,28 @@
+import {db} from "$lib/server/db";
+import {userTable} from "$lib/server/db/schema";
+import bcrypt from "bcrypt";
+import {eq} from "drizzle-orm";
+
+export async function createUser(username: string, email: string, password: string): Promise<number | null> {
+    const existingUser = await db
+        .select({id: userTable.id})
+        .from(userTable)
+        .where(eq(userTable.email, email));
+
+    if (existingUser.length >= 1) {
+        throw new Error("Ein Nutzer mit dieser E-Mail existiert bereits.");
+    }
+
+    const passwordHash = await bcrypt.hash(password, 10);
+
+    const [newUser] = await db
+        .insert(userTable)
+        .values({
+            email,
+            username,
+            passwordHash,
+        })
+        .returning({id: userTable.id});
+
+    return newUser.id;
+}
