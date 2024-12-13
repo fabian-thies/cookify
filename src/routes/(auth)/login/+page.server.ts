25711e030597322207fc5d279bb01c3ded7a30a0
fabianthies <44522183+fabian-thies@users.noreply.github.com>
2024-12-13 03:08:22 +0100

    feat: add user login

--- src/routes/(auth)/login/+page.server.ts
+++ src/routes/(auth)/login/+page.server.ts
@@ -1,16 +1,39 @@
 import {fail, redirect} from '@sveltejs/kit';
+import {getUserByEmail} from "$lib/server/db/queries/user";
+import bcrypt from "bcrypt";
+import {signJWT} from "$lib/server/utils/auth";
 
 export const actions = {
-    default: async ({request}) => {
+    default: async ({request, cookies}) => {
         const data = await request.formData();
-        const email = data.get('email');
-        const password = data.get('password');
+        const email = String(data.get('email'));
+        const password = String(data.get('password'));
 
         if (!email || !password) {
             return fail(400, {formError: {error: 'Bitte alle Felder ausfüllen.'}});
         }
 
-        return redirect(303, '/');
+        const user = await getUserByEmail(email);
+
+        if (!user) {
+            return fail(400, {formError: {error: 'Der Benutzername oder das Passwort ist falsch.'}})
+        }
+
+        const isPasswordValid = await bcrypt.compare(password, user?.passwordHash);
+
+        if (isPasswordValid) {
+            const token = await signJWT(user.id.toString(), user.username);
+
+            cookies.set('token', token, {
+                httpOnly: true,
+                maxAge: 60 * 60 * 24 * 7,
+                path: '/'
+            });
+
+            return redirect(303, '/');
+        }
+
+        return fail(400, {formError: {error: 'Der Benutzername oder das Passwort ist falsch.'}})
     }
 };
 
