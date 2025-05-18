9217c36e24fe0e6a0a3eed400e2e959b78b9d875
fabianthies <44522183+fabian-thies@users.noreply.github.com>
2025-05-19 01:43:53 +0200

    Refactor schema to remove relations and add saved_recipes table

--- src/lib/server/db/schema.ts
+++ src/lib/server/db/schema.ts
@@ -1,5 +1,5 @@
 import {integer, pgTable, serial, text, timestamp} from 'drizzle-orm/pg-core';
-import {relations} from 'drizzle-orm';
+import {primaryKey} from "drizzle-orm/pg-core/primary-keys";
 
 export const user = pgTable('user', {
     id: text('id').primaryKey(),
@@ -10,23 +10,11 @@ export const user = pgTable('user', {
     updatedAt: timestamp('updated_at', {withTimezone: true}).defaultNow().notNull(),
 });
 
-export const userRelations = relations(user, ({many}) => ({
-    recipes: many(recipe),
-    sessions: many(session)
-}));
-
 export const session = pgTable('session', {
     id: text('id').primaryKey(),
     userId: text('user_id').notNull().references(() => user.id),
     expiresAt: timestamp('expires_at', {withTimezone: true, mode: 'date'}).notNull()
 });
-
-export const sessionRelations = relations(session, ({one}) => ({
-    user: one(user, {
-        fields: [session.userId],
-        references: [user.id]
-    })
-}));
 
 export const recipe = pgTable('recipe', {
     id: serial('id').primaryKey(),
@@ -42,15 +30,15 @@ export const recipe = pgTable('recipe', {
     updatedAt: timestamp('updated_at', {withTimezone: true, mode: 'date'}).notNull().defaultNow()
 });
 
-export const recipeRelations = relations(recipe, ({one, many}) => ({
-    user: one(user, {
-        fields: [recipe.userId],
-        references: [user.id]
-    }),
-    ingredients: many(ingredient),
-    instructions: many(instructions),
-    recipeCategories: many(recipeCategory)
-}));
+export const saved_recipes = pgTable('saved_recipes', {
+        userId: text('user_id').notNull().references(() => user.id),
+        recipeId: integer('recipe_id').notNull().references(() => recipe.id),
+        createdAt: timestamp('created_at', {withTimezone: true, mode: 'date'}).notNull().defaultNow()
+    },
+    (table) => [
+        primaryKey({columns: [table.userId, table.recipeId]})
+    ]
+);
 
 export const ingredient = pgTable('ingredient', {
     id: serial('id').primaryKey(),
@@ -59,51 +47,22 @@ export const ingredient = pgTable('ingredient', {
     quantity: text('quantity').notNull()
 });
 
-export const ingredientRelations = relations(ingredient, ({one}) => ({
-    recipe: one(recipe, {
-        fields: [ingredient.recipeId],
-        references: [recipe.id]
-    })
-}));
-
 export const instructions = pgTable('instructions', {
     id: serial('id').primaryKey(),
     recipeId: integer('recipe_id').notNull().references(() => recipe.id),
     instruction: text('instruction').notNull()
 });
 
-export const instructionsRelations = relations(instructions, ({one}) => ({
-    recipe: one(recipe, {
-        fields: [instructions.recipeId],
-        references: [recipe.id]
-    })
-}));
-
 export const category = pgTable('category', {
     id: serial('id').primaryKey(),
     name: text('name').notNull(),
 });
-
-export const categoryRelations = relations(category, ({many}) => ({
-    recipeCategories: many(recipeCategory)
-}));
 
 export const recipeCategory = pgTable('recipe_category', {
     id: serial('id').primaryKey(),
     recipeId: integer('recipe_id').notNull().references(() => recipe.id),
     categoryId: integer('category_id').notNull().references(() => category.id),
 });
-
-export const recipeCategoryRelations = relations(recipeCategory, ({one}) => ({
-    recipe: one(recipe, {
-        fields: [recipeCategory.recipeId],
-        references: [recipe.id]
-    }),
-    category: one(category, {
-        fields: [recipeCategory.categoryId],
-        references: [category.id]
-    })
-}));
 
 export type Session = typeof session.$inferSelect;
 export type User = typeof user.$inferSelect;
@@ -112,3 +71,4 @@ export type Ingredient = typeof ingredient.$inferSelect;
 export type Instructions = typeof instructions.$inferSelect;
 export type Category = typeof category.$inferSelect;
 export type RecipeCategory = typeof recipeCategory.$inferSelect;
+export type SavedRecipe = typeof saved_recipes.$inferSelect;
