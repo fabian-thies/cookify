e4784bc27ba4453c2a953d7677ea36600564ef62
Fabian <thies.fab@gmail.com>
2025-07-28 00:10:11 +0200

    remove

--- src/lib/utils/debounce.ts
+++
@@ -1,7 +0,0 @@
-export function debounce(fn: (...args: any[]) => void, delay = 300) {
-    let timer: ReturnType<typeof setTimeout>;
-    return (...args: any[]) => {
-        clearTimeout(timer);
-        timer = setTimeout(() => fn(...args), delay);
-    };
-}
--- src/routes/(app)/+layout.svelte
+++
@@ -1,9 +0,0 @@
-<script>
-    import Nav from "$lib/components/Nav.svelte";
-
-    let {data, children} = $props();
-</script>
-
-<Nav data={data}/>
-
-{@render children()}
