88749e6dc8029e451b2114521c52582fc7a79458
Fabian <thies.fab@gmail.com>
2025-07-28 23:18:06 +0200

    feat: add login UI and theme customization

---
+++ src/routes/+page.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+    import LoginForm from "$lib/components/login-form.svelte";
+    import { Apple } from 'lucide-svelte';
+</script>
+<div class="bg-muted flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
+    <div class="flex w-full max-w-sm flex-col gap-6">
+        <a href="##" class="flex items-center gap-2 self-center font-medium">
+            <div
+                    class="bg-primary text-primary-foreground flex size-6 items-center justify-center rounded-md"
+            >
+                <Apple class="size-4" />
+            </div>
+            Cookify
+        </a>
+        <LoginForm />
+    </div>
+</div>
