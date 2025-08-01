8b1405a3b90843319ccbcbe7669733645efd9466
Fabian <thies.fab@gmail.com>
2025-08-02 01:32:38 +0200

    temp

---
+++ src/lib/components/ui/select/select.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+    import * as Select from "$lib/components/ui/select/index.js";
+
+    interface Props {
+        options: Record<string, string>;
+    }
+
+    let { options }: Props = $props();
+
+    let selected: string = $state("1");
+</script>
+
+<Select.Root type="single" bind:value={selected}>
+    <Select.Trigger>
+        {options[selected]}
+    </Select.Trigger>
+    <Select.Content>
+        {#each options as option, key}
+            <Select.Item value={option.value}>{option}</Select.Item>
+        { /each }
+    </Select.Content>
+</Select.Root>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+    import * as Select from "$lib/components/ui/select/index.js";
+
+    let selected: string = $state("1");
+
+    const options: Record<string, string> = {
+        "1": "Alle Kategorien",
+        "2": "Light",
+        "3": "Dark",
+        "4": "System"
+    };
+</script>
+
+<Select.Root type="single" bind:value={selected}>
+    <Select.Trigger class="w-[180px]">
+        {options[selected]}
+    </Select.Trigger>
+    <Select.Content>
+        <Select.Item value="1">Alle Kategorien</Select.Item>
+        <Select.Item value="2">Light</Select.Item>
+        <Select.Item value="3">Dark</Select.Item>
+        <Select.Item value="4">System</Select.Item>
+    </Select.Content>
+</Select.Root>
