2b1a7834ee63797ff15561c4cb07f0004f0d1cc0
Fabian <thies.fab@gmail.com>
2025-09-08 00:29:17 +0200

    feat: integrate carousel for suggested recipes

--- src/lib/components/recipe/SuggestedRecipes.svelte
+++ src/lib/components/recipe/SuggestedRecipes.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
     import RecipeCard from "$lib/components/recipe/RecipeCard.svelte";
+    import * as Carousel from "$lib/components/ui/carousel/index.js";
 
     type Props = {
         title: string;
@@ -14,13 +15,21 @@
         { title }
     </h2>
     <div class="flex justify-between gap-6">
-        <RecipeCard title="Title" description="Description" image="https://picsum.photos/1500/1500" serves={4}
-                    cookingTime={20} rating={4.5}/>
-        <RecipeCard title="Title" description="Description" image="https://picsum.photos/1500/1500" serves={4}
-                    cookingTime={20} rating={4.5}/>
-        <RecipeCard title="Title" description="Description" image="https://picsum.photos/1500/1500" serves={4}
-                    cookingTime={20} rating={4.5}/>
-        <RecipeCard title="Title" description="Description" image="https://picsum.photos/1500/1500" serves={4}
-                    cookingTime={20} rating={4.5}/>
+        <Carousel.Root class="w-full flex justify-center">
+            <Carousel.Content class="-ml-1">
+                {#each Array(5) as _, i (i)}
+                    <Carousel.Item class="pl-1 md:basis-1/2 lg:basis-1/3">
+                        <div class="p-1">
+                            <RecipeCard title="Title" description="Description" image="https://picsum.photos/1500/1500"
+                                        serves={4}
+                                        cookingTime={20} rating={4.5}/>
+                        </div>
+                    </Carousel.Item>
+                {/each}
+            </Carousel.Content>
+
+            <Carousel.Previous />
+            <Carousel.Next />
+        </Carousel.Root>
     </div>
 </div>
