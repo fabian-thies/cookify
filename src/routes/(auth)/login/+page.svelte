4b243456047ffe4fceffb6758bacb2a02e684e0c
Fabian <thies.fab@gmail.com>
2025-09-22 04:33:22 +0200

    feat: add padding to the login page

--- src/routes/(auth)/login/+page.svelte
+++ src/routes/(auth)/login/+page.svelte
@@ -2,7 +2,7 @@
     import LoginForm from "$lib/components/LoginForm.svelte";
     import {Apple} from 'lucide-svelte';
 </script>
-<div class="flex min-h-screen flex-col items-center justify-center gap-6">
+<div class="flex min-h-screen flex-col items-center justify-center gap-6 px-4 md:px-0">
     <div class="flex w-full max-w-sm flex-col gap-6">
         <a href="##" class="flex items-center gap-2 self-center font-medium">
             <div
