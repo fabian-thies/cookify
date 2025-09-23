602f5e009e1d766800536f1fa01726725df4ce5c
Fabian <thies.fab@gmail.com>
2025-09-23 02:57:40 +0200

    feat: add delete recipe functionality

--- src/lib/functions/recipe.remote.ts
+++ src/lib/functions/recipe.remote.ts
@@ -1,6 +1,6 @@
 import * as v from 'valibot';
 import {command, getRequestEvent} from '$app/server';
-import {toggleRecipeFavorite} from "$lib/server/db/recipe";
+import {toggleRecipeFavorite, deleteRecipe as deleteRecipeFromDb} from '$lib/server/db/recipe';
 
 export const likeRecipe = command(v.object({
     recipeId: v.number()
@@ -13,4 +13,17 @@ export const likeRecipe = command(v.object({
     }
 
     return await toggleRecipeFavorite(userId, recipeId);
+});
+
+export const deleteRecipe = command(v.object({
+    recipeId: v.number()
+}), async ({recipeId}) => {
+    const {locals} = getRequestEvent();
+    const userId = locals.user?.id;
+
+    if (!userId) {
+        throw new Error('User not authenticated');
+    }
+
+    return await deleteRecipeFromDb(userId, recipeId);
 });
