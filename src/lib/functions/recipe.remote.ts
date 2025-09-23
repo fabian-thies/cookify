942425f489a603b7f77613aa04cbda8854833919
Fabian <thies.fab@gmail.com>
2025-09-23 02:43:18 +0200

    fix: retrieve userId from request event in likeRecipe command

--- src/lib/functions/recipe.remote.ts
+++ src/lib/functions/recipe.remote.ts
@@ -1,10 +1,16 @@
 import * as v from 'valibot';
-import {command} from '$app/server';
+import {command, getRequestEvent} from '$app/server';
 import {toggleRecipeFavorite} from "$lib/server/db/recipe";
 
 export const likeRecipe = command(v.object({
-    userId: v.string(),
     recipeId: v.number()
-}), async ({userId, recipeId}) => {
+}), async ({recipeId}) => {
+    const {locals} = getRequestEvent();
+    const userId = locals.user?.id;
+
+    if (!userId) {
+        throw new Error('User not authenticated');
+    }
+
     return await toggleRecipeFavorite(userId, recipeId);
 });
