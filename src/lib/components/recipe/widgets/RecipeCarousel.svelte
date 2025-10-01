69b16e4332ee12b381d6cbedb4747ea52ebe5f89
winact <thies.fab@gmail.com>
2025-10-02 00:43:58 +0200

    feat: implement RecipeCarousel.svelte

--- src/lib/components/recipe/widgets/RecipeCarousel.svelte
+++ src/lib/components/recipe/widgets/RecipeCarousel.svelte
@@ -1,13 +1,18 @@
 <script lang="ts">
     import RecipeCard from "$lib/components/recipe/widgets/RecipeCard.svelte";
     import * as Carousel from "$lib/components/ui/carousel";
+    import type {HTMLAttributes} from "svelte/elements";
+    import type {ComponentProps} from "svelte";
+    import type {Recipe} from "$lib/server/db/schema";
+    import { m } from "$lib/paraglide/messages";
 
     type Props = {
         title: string;
         class?: string;
+        recipes: Recipe[];
     }
 
-    const {class: className, title}: Props = $props();
+    const {class: className, title, recipes}: Props = $props();
 </script>
 
 <div class={`w-full ${className}`}>
@@ -15,21 +20,25 @@
         { title }
     </h2>
     <div class="flex justify-between gap-6">
-        <Carousel.Root class="w-full flex justify-center">
-            <Carousel.Content class="-ml-1">
-                {#each Array(5) as _, i (i)}
-                    <Carousel.Item class="pl-1 md:basis-1/2 lg:basis-1/3">
-                        <div class="p-1">
-                            <RecipeCard id={2} title="Title" description="Description"
-                                        image="https://picsum.photos/1500/1500"
-                                        servings={4}
-                                        cookingTime={20} difficulty="easy"/>
-                        </div>
-                    </Carousel.Item>
-                {/each}
-            </Carousel.Content>
-            <Carousel.Previous />
-            <Carousel.Next />
-        </Carousel.Root>
+        {#if (recipes.length > 0)}
+            <Carousel.Root class="w-full flex justify-center">
+                <Carousel.Content class="-ml-1">
+                    {#each recipes as recipe}
+                        <Carousel.Item class="pl-1 md:basis-1/2 lg:basis-1/3">
+                            <div class="p-1">
+                                <RecipeCard id={recipe.id} title={recipe.title} description={recipe.description}
+                                            image={recipe.image}
+                                            servings={recipe.servings}
+                                            cookingTime={recipe.cookingTime} difficulty="easy"/>
+                            </div>
+                        </Carousel.Item>
+                    {/each}
+                </Carousel.Content>
+                <Carousel.Previous />
+                <Carousel.Next />
+            </Carousel.Root>
+        {:else}
+            <p class="text-muted-foreground text-md">{m["recipe.list.noRecipes"]()}</p>
+        {/if}
     </div>
 </div>
