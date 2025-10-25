e03378602afb1c0eb85e4bc7a7294e8ff7d41742
Fabian <thies.fab@gmail.com>
2025-10-25 05:29:05 +0200

    feat: Add no collections message

--- src/routes/(app)/collection/+page.svelte
+++ src/routes/(app)/collection/+page.svelte
@@ -1,11 +1,14 @@
 <script lang="ts">
 import CollectionCard from "$lib/components/collection/CollectionCard.svelte";
 import {getCollections} from "$lib/functions/recipe.remote";
+import {m} from "$lib/paraglide/messages";
 
 </script>
 
-<div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6 place-items-center">
+<div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6 place-items-center mt-6 pt-4">
     {#each await getCollections() as {title, recipeCount, id, thumbnails} }
         <CollectionCard title={title} images={thumbnails} collectionId={id} recipeCount={recipeCount}/>
+    {:else}
+        <p class="col-span-full text-center text-muted-foreground">{m["collection.noCollections"]()}</p>
     {/each }
 </div>
