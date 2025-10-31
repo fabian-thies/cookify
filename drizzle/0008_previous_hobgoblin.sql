6e9e46ea8ede13046040d4fc8520743016207385
Fabian <thies.fab@gmail.com>
2025-10-31 16:06:24 +0100

    feat: Add site_settings table

---
+++ drizzle/0008_previous_hobgoblin.sql
@@ -0,0 +1,7 @@
+CREATE TABLE "site_settings" (
+	"id" serial PRIMARY KEY NOT NULL,
+	"key" text NOT NULL,
+	"value" text NOT NULL,
+	"updated_at" timestamp with time zone DEFAULT now() NOT NULL,
+	CONSTRAINT "site_settings_key_unique" UNIQUE("key")
+);
